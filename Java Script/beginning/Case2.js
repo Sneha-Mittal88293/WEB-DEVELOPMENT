6aa96cd8bb09baf24667d04b405dadd6c410013a
sneha <snehamittal88293@gmail.com>
2022-09-28 18:45:47 +0530

    only convert 1 letter in upper case

--- Java Script/beginning/Case2.js
+++ Java Script/beginning/Case2.js
@@ -1,3 +1,3 @@
 const prompt = require("prompt-sync")();
 var a = prompt();
-console.log((a.slice(0,1)).toUpperCase() + a.slice(1,));
+console.log((a.slice(0,1)).toUpperCase() + (a.slice(1,)).toLowerCase());
