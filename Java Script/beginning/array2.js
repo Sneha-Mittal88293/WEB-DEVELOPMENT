f9f384c0dca813a0c560909f8cbb7c1d3aba7650
sneha <snehamittal88293@gmail.com>
2022-09-29 22:23:45 +0530

    print fizz when no. divisible 3 , print buzz when no. divisible by 5 , print fizzbuzz when divisible by 3 and 5 both otherwise print the number

---
+++ Java Script/beginning/array2.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+var a = [];
+
+
+for(var i = 1 ; i<=100; i++){
+    if(i%3 === 0 && i%5 === 0){
+        console.log("Fizzbuzz");
+    }
+    else if(i%3 === 0){
+        console.log("Fizz");
+    }
+    else if(i%5 === 0){
+        console.log("buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
