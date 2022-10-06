81bf2e81ec82d3abb95915ccdb7c77132597522c
sneha <snehamittal88293@gmail.com>
2022-10-06 15:33:27 +0530

    structure for apply jquery

---
+++ jQuery/index.js
@@ -0,0 +1,20 @@
+// Apply css
+$("h1").css("color","red");
+
+
+// Add claas
+$("h1").addClass("big_title second_class");
+
+// Remove class
+$("h1").removeClass("second_class");
+
+// Change text
+$("h1").text("Hello World");
+$("a").text("You Tube");
+
+// Change text also html style
+$("h2").html("<em>Hello World</em>");
+
+// Change attribute
+$("img").attr("alt" ,"drum");
+$("a").attr("href" , "https://youtube.com");
