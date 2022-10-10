5682e7abe2ca65e3d2a7a5eda93cee2556e7cec4
sneha <snehamittal88293@gmail.com>
2022-10-10 12:04:42 +0530

    learn about node

--- intro-to-node/hello.js
+++ intro-to-node/hello.js
@@ -3,3 +3,7 @@ console.log("hello world");
 const fs =require("fs");
 
 fs.copyFileSync("file1.txt" , "file2.txt");
+
+var superheroes = require("superheroes");
+var myhero = superheroes.random();
+console.log(myhero);
