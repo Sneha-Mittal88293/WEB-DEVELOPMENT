8de28955e3ea226805db5c92b428b206cdb099bf
sneha <snehamittal88293@gmail.com>
2022-10-10 18:11:22 +0530

    first server

--- express-server/server.js
+++ express-server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+//on get first argument is our location and second is your function inside function there is two argument one is request and second is response
+
 app.get("/" , function(request , response){
     response.send("hello");
 });
