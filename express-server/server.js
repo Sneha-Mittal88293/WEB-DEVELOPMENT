60714cec73b5d0d44ba84a4e49a444c137b92d8e
sneha <snehamittal88293@gmail.com>
2022-10-11 11:44:10 +0530

    first server

--- express-server/server.js
+++ express-server/server.js
@@ -4,8 +4,18 @@ const app = express();
 
 //on get first argument is our location and second is your function inside function there is two argument one is request and second is response
 
+//home route
 app.get("/" , function(request , response){
     response.send("hello");
+});
+
+// contact route
+app.get("/contact" , function(req , res){
+    res.send("Contact me as: - snehamittal87@gmail.com");
+});
+// about
+app.get("/about" , function(req , res){
+    res.send("my name is sneha mittal , i am btech student");
 });
 
 app.listen(3000 , function(){
